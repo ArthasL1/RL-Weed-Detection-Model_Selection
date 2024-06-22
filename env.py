910bdb3a97e762184667f4fad0c770fd8d4b95dc
ArthasL1 <zeqil1@uci.edu>
2024-06-23 01:10:54 +0800

    Update Eval and Config

--- env.py
+++ env.py
@@ -22,10 +22,10 @@ class WeedDetectionEnv(gym.Env):
         self.action_space = spaces.Discrete(4)
 
         # Define observation space
-        # Image input: 640 x 640 pixels (0-255, uint8)
-        image_space = spaces.Box(low=0, high=255, shape=(640, 640), dtype=np.uint8)
+        # Image input: 640 x 640 x 3 pixels (0-255, uint8)
+        image_space = spaces.Box(low=0, high=255, shape=(640, 640, 3), dtype=np.uint8)
         # Remaining energy: continuous space but integer values from 0 to 100
-        energy_space = spaces.Box(low=0, high=100, shape=(1,), dtype=np.float32)
+        energy_space = spaces.Box(low=0, high=120, shape=(1,), dtype=np.float32)
         # Remaining images to process: continuous space but integer values from 0 to 10
         image_count_space = spaces.Box(low=0, high=10, shape=(1,), dtype=np.float32)
 
@@ -40,9 +40,9 @@ class WeedDetectionEnv(gym.Env):
         self.ini_energy = 120
         self.ini_image_left = 10
         self.state = {
-            'image': np.random.randint(0, 256, (640, 640), dtype=np.uint8),
-            'energy': self.ini_energy,
-            'image_left': self.ini_image_left
+            'image': np.random.randint(0, 256, (640, 640, 3), dtype=np.uint8),
+            'energy': np.array([self.ini_energy], dtype=np.float32),
+            'image_left': np.array([self.ini_image_left], dtype=np.float32)
         }
         self.obs = self.state
         self.reward = 0
@@ -50,17 +50,19 @@ class WeedDetectionEnv(gym.Env):
         # Useful parameter
         self.total_steps = 0
         self.total_episodes = 0
-        self.episode_in_process = False #
-        self.done_episode = False
+        self.info = {}
+        self.episode_in_process = False  # If the current episode has proceeded some steps but not yet achieved a "done"
+        self.done_episode = False  # If the current episode has just achieved a "done"
+        self.in_eval = False  # If the environment is in evaluation
 
     def step(self, action):
         self.total_steps += 1
         self.episode_in_process = True
         self.done_episode = False
         # Update environment state based on action
-        self.state['image'] = np.random.randint(0, 256, (640, 640), dtype=np.uint8)
-        self.state['energy'] = max(0, self.state['energy'] - (action + 1)*10)
-        self.state['image_left'] = max(0, self.state['image_left'] - 1)
+        self.state['image'] = np.random.randint(0, 256, (640, 640, 3), dtype=np.uint8)
+        self.state['energy'] = np.array([max(0, self.state['energy'][0] - (action + 1)*10)], dtype=np.float32)
+        self.state['image_left'] = np.array([max(0, self.state['image_left'][0] - 1)], dtype=np.float32)
 
         # Calculate reward
         self.reward = 5 + 5 * (action + 1)
@@ -86,9 +88,9 @@ class WeedDetectionEnv(gym.Env):
     def reset(self, seed=None, options=None):
         # Reset environment state and return initial observation
         self.state = {
-            'image': np.random.randint(0, 256, (640, 640), dtype=np.uint8),
-            'energy': self.ini_energy,
-            'image_left': self.ini_image_left
+            'image': np.random.randint(0, 256, (640, 640, 3), dtype=np.uint8),
+            'energy': np.array([self.ini_energy], dtype=np.float32),
+            'image_left': np.array([self.ini_image_left], dtype=np.float32)
         }
         self.obs = self.state
         self.info = {}
--- train_RL.py
+++ train_RL.py
@@ -10,19 +10,39 @@ import os
 from stable_baselines3.common.callbacks import EvalCallback, StopTrainingOnRewardThreshold
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.callbacks import BaseCallback
+from config import parse_arguments
 
-class RewardCallback(BaseCallback):
-    def __init__(self, current_env, RL_n_steps, verbose=0):
-        super(RewardCallback, self).__init__(verbose)
+
+class RewardEvalCallback(BaseCallback):
+    def __init__(self, current_env, eval_env, RL_n_steps, eval_freq, save_path, verbose=0):
+        super(RewardEvalCallback, self).__init__(verbose)
         self.current_env = current_env
+        self.eval_env = eval_env
+        self.eval_freq = eval_freq
+        self.RL_n_steps = RL_n_steps
+        self.save_path = save_path
+
         self.episode_rewards = []
         self.episode_steps = []
         self.current_rewards = 0
         self.current_step = 0
+        self.eval_mean_rewards = []
+        self.eval_std_rewards = []
+        self.eval_mean_episode_length = []
+        self.eval_std_episode_length = []
+        self.eval_count = 0
+        self.best_mean_reward = float('-inf')
+        self.best_std_reward = 0
+        self.best_eval_count = 0
 
     def _on_step(self) -> bool:
         self.current_rewards += self.locals['rewards'][0]
         self.current_step += 1
+
+        # Check if the model should be evaluated
+        if self.current_env.total_steps % self.eval_freq == 1:
+            self.update_eval()
+
         # Check if the episode has ended
         if self.current_env.done_episode:
             self.episode_rewards.append(self.current_rewards)
@@ -31,42 +51,129 @@ class RewardCallback(BaseCallback):
             self.current_step = 0
         return True
 
-def train_RL(env, model_name, save_path, model_type="PPO", learn_timesteps=20480, RL_n_steps=2048):
+    def update_eval(self):
+        self.eval_count += 1
+        mean_reward, std_reward, mean_episode_length, std_episode_length = eval_RL(eval_env=self.eval_env,
+                                                                                   model=self.model)
+        print(f"Evaluation {self.eval_count}: \nMean Reward = {mean_reward}; STD = {std_reward}")
+        self.eval_mean_rewards.append(mean_reward)
+        self.eval_std_rewards.append(std_reward)
+        self.eval_mean_episode_length.append(mean_episode_length)
+        self.eval_std_episode_length.append(std_episode_length)
+
+        # Save the best model if found
+        if mean_reward > self.best_mean_reward:
+            self.best_mean_reward = mean_reward
+            self.best_std_reward = std_reward
+            self.best_eval_count = self.eval_count
+            print("Current Best Reward!")
+            self.model.save(self.save_path + "best_model")
+
+
+def eval_RL(eval_env, model, eval_episodes=20, deterministic=False):
+    eval_env.in_eval = True
+    all_episode_rewards = []
+    all_episode_length = []
+    for episode in range(eval_episodes):
+        obs = eval_env.reset()[0]
+        episode_reward = 0
+        episode_steps_survived = 0
+        while True:
+            act = model.predict(obs, deterministic=deterministic)[0]
+            obs, reward, terminated, truncated, info = eval_env.step(act)
+            episode_reward += reward
+            episode_steps_survived += 1
+            if terminated or truncated:
+                break
+        all_episode_rewards.append(episode_reward)
+        all_episode_length.append(episode_steps_survived)
+    mean_reward = np.mean(all_episode_rewards)
+    std_reward = np.std(all_episode_rewards)
+    mean_episode_length = np.mean(all_episode_length)
+    std_episode_length = np.std(all_episode_length)
+    # print(f"Mean Reward = {mean_reward}; STD = {std_reward}")
+    eval_env.in_eval = False
+    return mean_reward, std_reward, mean_episode_length, std_episode_length
+
+
+
+
+
+
+def train_RL(train_env, eval_env, model_name, save_path, model_type="PPO", learn_timesteps=20480, RL_n_steps=2048):
 
     # Define RL model
     model = None
     if model_type == "PPO":
-        model = PPO('MultiInputPolicy', env, verbose=0, n_steps=RL_n_steps)
+        model = PPO('MultiInputPolicy', train_env, verbose=0, n_steps=RL_n_steps)
     else:
         print(f"The model type {model_type} is not defined!")
 
-    reward_callback = RewardCallback(current_env=env, RL_n_steps=RL_n_steps)
-    model.learn(total_timesteps=learn_timesteps, callback=reward_callback)
+    reward_eval_callback = RewardEvalCallback(current_env=train_env, eval_env=eval_env, RL_n_steps=RL_n_steps,
+                                              eval_freq=RL_n_steps, save_path=save_path)
+    model.learn(total_timesteps=learn_timesteps, callback=reward_eval_callback)
 
+    # Evaluated the final model (which is not evaluated during callback)
+    reward_eval_callback.update_eval()
+
+    # Save the last model (optional, maybe unnecessary)
     model.save(save_path + "final_model")
+
     # Plot the learning curve
-    plt.plot(reward_callback.episode_rewards)
+    plt.plot(reward_eval_callback.episode_rewards)
     plt.xlabel('Episode')
     plt.ylabel('Reward')
-    plt.title('Learning Curve')
-    plt.savefig(save_path + "rewards_plot.png")
+    plt.title(f'{model_type} Learning Curve: Rewards')
+    plt.savefig(save_path + "train_rewards_plot.png")
     plt.clf()
 
-    plt.plot(reward_callback.episode_steps)
+    plt.plot(reward_eval_callback.episode_steps)
     plt.xlabel('Episode')
     plt.ylabel('Steps')
-    plt.title('Steps Curve')
-    plt.savefig(save_path + "steps_plot.png")
+    plt.title(f'{model_type} Learning Curve: Length')
+    plt.savefig(save_path + "train_length_plot.png")
     plt.clf()
 
+    # Plot the evaluation curve
+    mean_reward_np = np.asarray(reward_eval_callback.eval_mean_rewards)
+    std_reward_np = np.asarray(reward_eval_callback.eval_std_rewards)
+    plt.title(f"{model_type} Evaluation Curve: Rewards")
+    plt.fill_between(range(len(mean_reward_np)), mean_reward_np - std_reward_np, mean_reward_np +
+                     std_reward_np, alpha=0.2)
+    plt.plot(range(1, len(mean_reward_np) + 1), mean_reward_np)
+    plt.xlabel(f"Evaluation Count")
+    plt.ylabel("Mean Reward")
+    plt.savefig(save_path + "eval_rewards_plot.png")
+    plt.clf()
+
+    mean_episode_np = np.asarray(reward_eval_callback.eval_mean_episode_length)
+    std_episode_np = np.asarray(reward_eval_callback.eval_std_episode_length)
+    plt.title(f"{model_type} Evaluation Curve: Rewards")
+    plt.fill_between(range(len(mean_episode_np)), mean_episode_np - std_episode_np, mean_episode_np +
+                     std_episode_np, alpha=0.2)
+    plt.plot(range(1, len(mean_episode_np) + 1), mean_episode_np)
+    plt.xlabel(f"Evaluation Count")
+    plt.ylabel("Mean Reward")
+    plt.savefig(save_path + "eval_rewards_plot.png")
+    plt.clf()
 
 
 if __name__ == "__main__":
-    env = WeedDetectionEnv()
-    model_name = "PPO_test0"
+
+    train_env = WeedDetectionEnv()
+    eval_env = WeedDetectionEnv()
+
+    # Get arguments from command line
+    args = parse_arguments()
+    model_name = args.model_name  # PPO_test0
+    model_type = args.model_type  # PPO
+    learn_timesteps = args.learn_timesteps  # 2560
+    RL_n_steps = args.RL_n_steps  # 256
+
     save_path = f"./RL_models/{model_name}/"
     os.makedirs(save_path, exist_ok=True)
-    train_RL(env, model_name, save_path, model_type="PPO", learn_timesteps=20480, RL_n_steps=2048)
+    train_RL(train_env, eval_env, model_name, save_path, model_type=model_type, learn_timesteps=learn_timesteps,
+             RL_n_steps=RL_n_steps)
 
 
 
